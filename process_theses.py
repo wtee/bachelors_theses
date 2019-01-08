718078b267a2152895ba8aecd1e0eee66c438961
wtee <wtee@users.noreply.github.com>
2019-01-07 22:48:27 -0600

    Add truecaser.py and deslash_title

--- process_theses.py
+++ process_theses.py
@@ -1,4 +1,10 @@
 import openpyxl
+
+import truecaser
+
+truecaser_model_file = "resources/distributions.obj"
+truecaser_model = truecaser.load_model(truecaser_model_file)
+
 # Get spreadsheet from Box (not implemented)
 
 # load spreadsheet
@@ -7,19 +13,23 @@ wb = openpyxl.load_workbook(sheet)
 
 
 def make_245_c(author_statement):
-    deslashed = deslash(author_statement)
-    #trucased = trucase(deslashed)
+    deslashed = deslash_author_statement(author_statement)
+    truecased = truecase(deslashed)
+    if truecased.startswith("By"):
+        output = "".join(["by", truecased[2:]])
+    else:
+        output = truecased
 
-    return deslashed
+    return output
 
-def deslash(str_):
+def deslash_author_statement(str_):
     """Convert a string like by J. P. Anderson \ A. H. Boileau \ W. E. Dexter \ J. A. Yungclas."
     to one like "by J. P. Anderson, A. H. Boileau, W. E. Dexter, and J. A. Yungclas."
     """
     if (str_.count("\\") == 1):
         deslashed = str_.replace("\\", " and ")
     elif (str_.count("\\") > 1):
-        temp = str_.replace("\\", ",").replace(" ,", ",")
+        temp = str_.replace("\\", ", ").replace(" ,", ",")
         # Replace the last comma with an and
         deslashed = " and ".join(
             [temp[: temp.rfind(",") + 1], temp[temp.rfind(",") + 1 :]]
@@ -29,6 +39,14 @@ def deslash(str_):
 
     # Along the way extra spaces may have been introduced.
     # Make sure to reduce those down to one space.
-    return deslashed.replace("  ", " ")
+    return deslashed.replace("  ", " ").replace("  ", " ")
 
+
+def deslash_title(str_):
+    return str_.replace("\\", "").replace("  ", " ")
+
+
+def truecase(str_):
+    tokens = str_.split(" ")
+    return " ".join(truecaser.get_true_case(tokens, "", truecaser_model))
 
--- tests/tests.py
+++ tests/tests.py
@@ -1,10 +1,55 @@
 import unittest
 
-from process_theses import deslash
+from process_theses import deslash_author_statement, deslash_title, truecase
 
 class UtilsTests(unittest.TestCase):
-    def test_deslash(self):
+    def test_deslash_author_statement(self):
         test_string = "by J. P. Anderson \ A. H. Boileau \ W. E. Dexter \ J. A. Yungclas."
         desired = "by J. P. Anderson, A. H. Boileau, W. E. Dexter, and J. A. Yungclas."
-        result = deslash(test_string)
+        result = deslash_author_statement(test_string)
         self.assertEqual(result, desired)
+    
+    def test_deslash_title(self):
+        test_string = "A Genetic Analysis \ of \ a Rabbit Stock as regards the \ Color Factor"
+        desired = "A Genetic Analysis of a Rabbit Stock as regards the Color Factor"
+        result = deslash_title(test_string)
+        self.assertEqual(result, desired)
+
+    def test_trucase_title(self):
+        test_string1 = "HISTORY OF THE TYPE CHANGE OF THE POLAND-CHINA BREED."
+        test_string2 = "A STUDY OF THE DECOMPOSITION OF GREEN MANURES."
+        test_string3 = "SEASONAL PROFILE VARIATION OF AN EARTH ROAD."
+        test_string4 = "An Investigation of Industrial Housing"
+
+        desired1 = "History of the type change of the Poland-China Breed."
+        desired2 = "A study of the decomposition of green manures."
+        desired3 = "Seasonal profile variation of an earth road."
+        desired4 = "An investigation of industrial housing"
+
+        result1 = truecase(desired1)
+        result2 = truecase(desired2)
+        result3 = truecase(desired3)
+        result4 = truecase(desired4)
+
+        self.assertEqual(result1, desired1)
+        self.assertEqual(result2, desired2)
+        self.assertEqual(result3, desired3)
+        self.assertEqual(result4, desired4)
+
+
+    def test_trucase_author_statement(self):
+        test_string1 = "by HERBERT CHARLES FLINT and WILLIAM FRANCIS LAGRANGE"
+        test_string2 = "BY OTMAR W. ZACK"
+        test_string3 = "by A. A. BAUSTIAN, H. D. SUSONG, and E. YOUNG"
+
+        desired1 = "By Herbert Charles Flint and William Francis Lagrange"
+        desired2 = "By Otmar W. Zack"
+        desired3 = "By A. A. Baustian, H. D. Susong, and E. Young"
+
+        result1 = truecase(desired1)
+        result2 = truecase(desired2)
+        result3 = truecase(desired3)
+
+        self.assertEqual(result1, desired1)
+        self.assertEqual(result2, desired2)
+        self.assertEqual(result3, desired3)
