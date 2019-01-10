6659991da5c8e164991b66f1255ef9863dd2f46e
wtee <wtee@users.noreply.github.com>
2019-01-09 22:08:16 -0600

    Add needs_truecase, final_styling_for_title, final_styling_for_author, make_245_c, make_245_a

--- process_theses.py
+++ process_theses.py
@@ -1,6 +1,7 @@
 import openpyxl
 
 import truecaser
+from fuzzywuzzy import fuzz
 
 truecaser_model_file = "resources/distributions.obj"
 truecaser_model = truecaser.load_model(truecaser_model_file)
@@ -11,16 +12,26 @@ truecaser_model = truecaser.load_model(truecaser_model_file)
 sheet = "bachelor's thesis precat worksheet.xlsx"
 wb = openpyxl.load_workbook(sheet)
 
+def make_245_a(title):
+    deslashed = deslash_title(title)
+    if needs_truecase(deslashed, "title"):
+        truecased = truecase(deslashed)
+    else:
+        truecased = deslashed
+    styled = final_styling_for_title(truecased)
+
+    return styled
+
 
 def make_245_c(author_statement):
     deslashed = deslash_author_statement(author_statement)
-    truecased = truecase(deslashed)
-    if truecased.startswith("By"):
-        output = "".join(["by", truecased[2:]])
+    if needs_truecase(deslashed, "author"):
+        truecased = truecase(deslashed)
     else:
-        output = truecased
+        truecased = deslashed
+    styled = final_styling_for_author(truecased)
 
-    return output
+    return styled
 
 def deslash_author_statement(str_):
     """Convert a string like by J. P. Anderson \ A. H. Boileau \ W. E. Dexter \ J. A. Yungclas."
@@ -48,5 +59,65 @@ def deslash_title(str_):
 
 def truecase(str_):
     tokens = str_.split(" ")
-    return " ".join(truecaser.get_true_case(tokens, "", truecaser_model))
+    return " ".join(truecaser.get_true_case(tokens, "title", truecaser_model))
+
+def needs_truecase(str_, type_):
+    """Checks if a string needs to be truecased.
+
+    Returns True or False depending on whether a string too closely resembles
+    an all-caps version of the string.
+
+    Parameters
+    ----------
+    str_ : str
+        The string to check.
+    type_ : str
+        What type of string is being checked. Accepts "title" or "author".
+
+    Returns
+    -------
+    bool
+    """
+    ratio = fuzz.ratio(str_, str_.upper())
+
+    if type_ == "title":
+        # "An Investigation of Industrial Housing" should return True.
+        # "A study of the decomposition of green manures." should return
+        # False.
+        if ratio > 20:
+            return_ = True
+        else:
+            return_ = False
+    elif type_ == "author":
+        # "by HERBERT CHARLES FLINT and WILLIAM FRANCIS LAGRANGE" 
+        # should return True.
+        # "By A. A. Baustian, H. D. Susong, and E. Young" should
+        # return False.
+        if ratio > 60:
+            return_ = True
+        else:
+            return_ = False
+
+    return return_
+
+
+def final_styling_for_title(str_):
+    # remove final "." if needed; end with " /"
+    styled_str = str_.rstrip(".")
+    styled_str += " /"
+
+    return styled_str
+
+
+def final_styling_for_author(str_):
+    #lowercase "By"; end with "."
+    if str_.startswith("By"):
+        styled_str = "by" + str_[2:]
+    else:
+        styled_str = str_
+
+    if not styled_str.endswith("."):
+        styled_str += "."
+
+    return styled_str
 
--- tests/tests.py
+++ tests/tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-from process_theses import deslash_author_statement, deslash_title, truecase
+from process_theses import deslash_author_statement, deslash_title, truecase, needs_truecase, final_styling_for_title, final_styling_for_author, make_245_c, make_245_a
 
 class UtilsTests(unittest.TestCase):
     def test_deslash_author_statement(self):
@@ -36,7 +36,6 @@ class UtilsTests(unittest.TestCase):
         self.assertEqual(result3, desired3)
         self.assertEqual(result4, desired4)
 
-
     def test_trucase_author_statement(self):
         test_string1 = "by HERBERT CHARLES FLINT and WILLIAM FRANCIS LAGRANGE"
         test_string2 = "BY OTMAR W. ZACK"
@@ -53,3 +52,58 @@ class UtilsTests(unittest.TestCase):
         self.assertEqual(result1, desired1)
         self.assertEqual(result2, desired2)
         self.assertEqual(result3, desired3)
+
+    def test_needs_truecase(self):
+        # Test authors
+        test_string1 = "by HERBERT CHARLES FLINT and WILLIAM FRANCIS LAGRANGE"
+        test_string2 = "By A. A. Baustian, H. D. Susong, and E. Young"
+        # Test titles
+        test_string3 = "An Investigation of Industrial Housing"
+        test_string4 = "A study of the decomposition of green manures."
+
+        self.assertTrue(needs_truecase(test_string1, "author"))
+        self.assertFalse(needs_truecase(test_string2, "author"))
+        self.assertTrue(needs_truecase(test_string3, "title"))
+        self.assertFalse(needs_truecase(test_string4, "title"))
+
+    def test_final_styling_for_title(self):
+        test_string = "A study of the decomposition of green manures."
+        desired = "A study of the decomposition of green manures /"
+
+        result = final_styling_for_title(test_string)
+
+        self.assertEqual(result, desired)
+    
+    def test_final_styling_for_author(self):
+        test_string = "By A. A. Baustian, H. D. Susong, and E. Young"
+        desired = "by A. A. Baustian, H. D. Susong, and E. Young."
+
+        result = final_styling_for_author(test_string)
+
+        self.assertEqual(result, desired)
+
+    def test_make_245_c(self):
+        test_string1 = "BY HERBERT CHARLES FLINT \ WILLIAM FRANCIS LAGRANGE"
+        test_string2 = "by J. P. Anderson \ A. H. Boileau \ W. E. Dexter \ J. A. Yungclas."
+
+        desired1 = "by Herbert Charles Flint and William Francis Lagrange."
+        desired2 = "by J. P. Anderson, A. H. Boileau, W. E. Dexter, and J. A. Yungclas."
+
+        result1 = make_245_c(test_string1)
+        result2 = make_245_c(test_string2)
+
+        self.assertEqual(result1, desired1)
+        self.assertEqual(result2, desired2)
+
+    def test_make_245_a(self):
+        test_string1 = "A Genetic Analysis \ of \ a Rabbit Stock as regards the \ Color Factor"
+        test_string2 = "COST ACCOUNT STUDY OF AN IOWA FARM \ 1918 - 1919."
+
+        desired1 = "A genetic analysis of a rabbit stock as regards the color factor /"
+        desired2 = "Cost account study of an Iowa farm 1918 - 1919 /"
+
+        result1 = make_245_a(test_string1)
+        result2 = make_245_a(test_string2)
+
+        self.assertEqual(result1, desired1)
+        self.assertEqual(result2, desired2)
